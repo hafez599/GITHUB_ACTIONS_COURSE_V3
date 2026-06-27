2024c34ae4c03d0595e1684619754379c728958a
hafez adel <103792766+hafez599@users.noreply.github.com>
2026-06-27 12:01:19 +0300

    Add print statement in run function

--- app/app.py
+++ app/app.py
@@ -62,6 +62,7 @@ class AppHandler(BaseHTTPRequestHandler):
 def run():
     server = HTTPServer(("0.0.0.0", 8000), AppHandler)
     print("Server is running on http://0.0.0.0:8000")
+    print("this from hafez")
     server.serve_forever()
 
 
